0c26a1e9e19d0a7a9e74d0ac0cfeccc929101a34
Elvis <ingatielvis632@gmail.com>
2024-11-11 14:12:32 +0300

    added more features

--- masizaafricawear/src/components/Productlist/Productlist.jsx
+++ masizaafricawear/src/components/Productlist/Productlist.jsx
@@ -1,9 +1,18 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { ShopContext } from '../Shopcontext/Shopcontext'
 import './Productlist.css'
+import { IoMdCloseCircle } from 'react-icons/io';
 
 function Productlist() {
-  const { products } = useContext(ShopContext)
+  const { products } = useContext(ShopContext);
+
+  const [modal, setModal] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState(null);
+
+  const toggleModal = (product = null) => {
+        setModal(!modal);
+        setSelectedProduct(product);
+  }
 
   return (
     <div>
@@ -18,7 +27,7 @@ function Productlist() {
             return (
               <div key={id} className="product-card">
 
-                <img src={image} alt={title} className='product-image' />
+                <img src={image} alt={title} className='product-image' onClick={()=> toggleModal(product)}/>
                 <div className="product_info">
                 <h3>{title}</h3>
                 <p>${price}</p>
@@ -31,6 +40,21 @@ function Productlist() {
           })}
         </div>
       </div>
+
+      {modal && selectedProduct && (
+        <div className="product_modal">
+        <div className="product_modal_content">
+        <IoMdCloseCircle className='modal_close' onClick={toggleModal} />
+           <div className="modal_content">
+            <img src={selectedProduct.image} className="modal_image" alt=""/>
+            <h3 className="modal_title">{selectedProduct.title}</h3>
+            <h2 className="modal_price">${selectedProduct.price}</h2>
+            <button className="buy_now">Buy Now</button>
+           </div>
+        </div>
+          
+        </div>
+      )}
     </div>
   )
 }
